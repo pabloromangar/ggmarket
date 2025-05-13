7fcabab0a527582138d7337c5ec2d74a96925b6c
Pablo Roman <romangarpablo@gmail.com>
2025-05-13 12:57:44 +0200

    first commit

---
+++ src/main/java/com/example/ggmarket/GgmarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.ggmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgmarketApplication.class, args);
+	}
+
+}
